736e83d3632e4c93c34d718c3ee2278dc5a353ee
Danielkatz23 <49025968+Danielkatz23@users.noreply.github.com>
2019-06-03 16:38:56 +0300

    Update Manager.java

--- src/user/manager/Manager.java
+++ src/user/manager/Manager.java
@@ -1,4 +1,67 @@
 package user.manager;
 
-public interface Manager {
+abstract Manager extends Worker{
+	private ContentEdit editor;
+	private ActivityReport reporter;
+	private DataArchive archiver;
+	
+	public Manager(String namePersonal_,String nameUser_,String password_,int phoneNumber_,String email_,ClientConsole client_){
+		super(namePersonal_,nameUser_,password_,phoneNumber_,email_,client_);
+		editor = new ContentEdit(client);
+		reporter = new ActivityReport(client);
+		archiver = new DataArchive()
+		Role = ManagerRole;
+	}
+	
+	public void viewReport(){
+		reporter.showReport();
+	}
+	
+	public void createCity(String cityName){
+		editor.createCity(cityName);
+	}
+	
+	public void editElectronicMap(int mapID){
+		editor.editElectronicMap(mapID);
+	}
+	
+	public void editCity(int cityID){
+		editor.editCity(cityID);
+	}
+	
+	public void addNewElectronicMapToCity(int cityID,ElectronicMap map){
+		editor.addNewElectronicMapToCity(cityID,map);
+	}
+	
+	public void addNewContent(int cityID,PlaceOfInterest content){
+		editor.addNewContent(cityID,content);
+	}
+	
+	public void editContent(int contentID){
+		editor.editContent(contentID);
+	}
+	
+	public void addNewRoute(){
+		editor.addNewRoute();
+	}
+	
+	public void editRoute(int routeID){
+		editor.editRoute(routeID);
+	}
+	
+	public void sendPublishVersion(){
+		editor.sendPublishVersion();
+	}
+	
+	public void newDateInterrupt(){
+		archiver.newDateInterrupt();
+	}
+	
+	public void createDailyReport(){
+		archiver.createDailyReport();
+	}
+	
+	public void addDailyReport(){
+		archiver.addDailyReport();
+	}
 }
