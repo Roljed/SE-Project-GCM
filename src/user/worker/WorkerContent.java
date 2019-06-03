43be590007655b616d18a93f7675f8ba777e00b5
Danielkatz23 <49025968+Danielkatz23@users.noreply.github.com>
2019-06-03 16:42:57 +0300

    Update and rename WorkerContent.java to ContentWorker.java

--- src/user/worker/WorkerContent.java
+++
@@ -1,9 +0,0 @@
-package user.worker;
-
-public class WorkerContent extends Worker
-{
-
-    public WorkerContent(String namePersonal_, String nameUser_, String password_, int phoneNumber_, String email_) {
-        super(namePersonal_, nameUser_, password_, phoneNumber_, email_);
-    }
-}
