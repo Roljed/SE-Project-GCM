64bfa60ea1f58c4936dba111a989cd83ce333588
Yaad Nahshon <ydnahshon@gmail.com>
2019-06-04 12:51:48 +0300

    1. Content class update. 2. Content Interesting Places update.

--- src/product/content/Content.java
+++ src/product/content/Content.java
@@ -12,6 +12,18 @@ public abstract class Content
     private int contendID;
     private Location locationCoordinate;
 
+    public Content(int newContentID, Location newLocationCoordinate)
+    {
+        this.contendID = newContentID;
+        this.locationCoordinate = newLocationCoordinate;
+    }
+
+    public Content(int newContentID, double newCoordinateX, double newCoordinateY)
+    {
+        this.contendID = newContentID;
+        this.locationCoordinate = new Location(newCoordinateX, newCoordinateY);
+    }
+
     public void setContentID(int newID)
     {
         this.contendID = newID;
