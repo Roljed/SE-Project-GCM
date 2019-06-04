71c4549c8483964020b756272785a3dd6fd4c3ab
Yaad Nahshon <ydnahshon@gmail.com>
2019-06-04 12:51:48 +0300

    1. Content class update. 2. Content Interesting Places update.

--- src/product/content/ContentInterestingPlaces.java
+++ src/product/content/ContentInterestingPlaces.java
@@ -1,6 +1,97 @@
 package product.content;
 
-public class ContentInterestingPlaces implements Content
+/**
+ * Implements interesting places on the DigitalMap
+ *
+ * @version 1
+ * @author Yaad Nahshon
+ */
+public class ContentInterestingPlaces extends Content
 {
+    private String name;
+    private Location location;
+    private Classification type;
+    private String details;
+    private boolean accessibility;
 
+    public enum Classification {
+        CINEMA,
+        HISTORICAL,
+        HOTEL,
+        MUSEUM,
+        PARK,
+        PUBLIC_SITE,
+        RESTURANT,
+        STORE
+    }
+
+    public ContentInterestingPlaces(int newContentID, Location newLocationCoordinate, String name, Location location, Classification type, String details, boolean accessibility)
+    {
+        super(newContentID, newLocationCoordinate);
+        this.name = name;
+        this.location = location;
+        this.type = type;
+        this.details = details;
+        this.accessibility = accessibility;
+    }
+
+    public ContentInterestingPlaces(int newContentID, double newCoordinateX, double newCoordinateY, String name, Location location, Classification type, String details, boolean accessibility)
+    {
+        super(newContentID, newCoordinateX, newCoordinateY);
+        this.name = name;
+        this.location = location;
+        this.type = type;
+        this.details = details;
+        this.accessibility = accessibility;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public Location getLocation()
+    {
+        return location;
+    }
+
+    public void setLocation(Location location)
+    {
+        this.location = location;
+    }
+
+    public Classification getType()
+    {
+        return type;
+    }
+
+    public void setType(Classification type)
+    {
+        this.type = type;
+    }
+
+    public String getDetails()
+    {
+        return details;
+    }
+
+    public void setDetails(String details)
+    {
+        this.details = details;
+    }
+
+    public boolean isAccessibility()
+    {
+        return accessibility;
+    }
+
+    public void setAccessibility(boolean accessibility)
+    {
+        this.accessibility = accessibility;
+    }
 }
