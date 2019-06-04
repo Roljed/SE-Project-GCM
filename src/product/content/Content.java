dfbfae38f9b52d21b13600d58f6443f08909ff3a
Yaad Nahshon <ydnahshon@gmail.com>
2019-06-04 12:20:37 +0300

    Content class update.

--- src/product/content/Content.java
+++ src/product/content/Content.java
@@ -1,6 +1,34 @@
 package product.content;
 
-public interface Content
+/**
+ * Abstract class for future proof content classes
+ *
+ * @version 1
+ * @author Yaad Nahshon
+ */
+public abstract class Content
 {
 
+    private int contendID;
+    private Location locationCoordinate;
+
+    public void setContentID(int newID)
+    {
+        this.contendID = newID;
+    }
+
+    public int getContendID()
+    {
+        return contendID;
+    }
+
+    public Location getLocationCoordinate()
+    {
+        return locationCoordinate;
+    }
+
+    public void setLocationCoordinate(Location locationCoordinate)
+    {
+        this.locationCoordinate = locationCoordinate;
+    }
 }
