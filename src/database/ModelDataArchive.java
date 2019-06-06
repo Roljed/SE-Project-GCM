817782fa84601f69fe924de9f1290a43f010e4ce
Yaad Nahshon <ydnahshon@gmail.com>
2019-06-06 18:44:00 +0300

    Data Archive must have variables.

--- src/database/ModelDataArchive.java
+++ src/database/ModelDataArchive.java
@@ -1,7 +1,18 @@
 package database;
 
+import product.City;
+import product.DigitalMap;
+import product.Tour;
+import product.content.Content;
+
+import java.util.HashMap;
+
 // Holds all product related data
 public class ModelDataArchive
 {
+    HashMap<Integer, Content> contentHub;
+    HashMap<Integer, DigitalMap> digitalMapHub;
+    HashMap<Integer, City> cityHub;
+    HashMap<Integer, Tour> tourHub;
 
 }
