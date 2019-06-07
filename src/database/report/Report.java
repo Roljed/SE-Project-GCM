5a4c3081c83a8b89ac61d7fcc5c905b4295f452d
avi <avi.ayeli@gmail.com>
2019-06-07 16:40:01 +0300

    Update: Report, ReportActivity

--- src/database/report/Report.java
+++ src/database/report/Report.java
@@ -1,12 +1,31 @@
 package database.report;
 
+import java.util.Date;
+
 /**
 *
 * @version 1
 * @author Avi Ayeli
 */
 
-public interface Report
+public abstract class Report
 {
+	private String activeManager;
+	private Date date;
+	
+	Report(String activeManager){
+		this.activeManager=activeManager;
+		date=new Date();
+	}
+	
+	public String getActiveManager() {
+		return activeManager;
+	}
+	
+	public Date getDate() {
+		return date;
+	}
+	
+	public abstract void PrintReport();
 
 }
