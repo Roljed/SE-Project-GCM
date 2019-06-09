e82bdf66cb8c81f90c200d277e217c6c59182297
Yaad Nahshon <ydnahshon@gmail.com>
2019-06-09 20:05:52 +0300

    Daniels Push.

--- src/database/ModelDataArchive.java
+++ src/database/ModelDataArchive.java
@@ -1,5 +1,6 @@
 package database;
 
+import command.Message;
 import product.City;
 import product.DigitalMap;
 import product.Tour;
@@ -8,7 +9,7 @@ import product.content.Content;
 import java.util.HashMap;
 
 // Holds all product related data
-public class ModelDataArchive
+public class ModelDataArchive implements Message
 {
     HashMap<Integer, Content> contentHub;
     HashMap<Integer, DigitalMap> digitalMapHub;
