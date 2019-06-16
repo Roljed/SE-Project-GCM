03b2edc118e6f1bed91643af1171493a35caf7e6
Yaad Nahshon <ydnahshon@gmail.com>
2019-06-16 13:56:34 +0300

    Daniel's updated and minor bugs :-)

---
+++ src/product/CityToDisplay.java
@@ -0,0 +1,69 @@
+package product;
+
+public class CityToDisplay
+{
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumberMap() {
+        return numberMap;
+    }
+
+    public void setNumberMap(String numberMap) {
+        this.numberMap = numberMap;
+    }
+
+    public String getNumberSite() {
+        return numberSite;
+    }
+
+    public void setNumberSite(String numberSite) {
+        this.numberSite = numberSite;
+    }
+
+    public String getNumberTour() {
+        return numberTour;
+    }
+
+    public void setNumberTour(String numberTour) {
+        this.numberTour = numberTour;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    private String name;
+    private String numberMap;
+    private String numberSite;
+    private String numberTour;
+    private String price;
+    private String description;
+
+    public CityToDisplay(String name, String numberMaps, String numberSites, String numberTour, String price, String description) {
+        this.name = name;
+        this.numberMap = numberMaps;
+        this.numberSite = numberSites;
+        this.numberTour = numberTour;
+        this.price = price;
+        this.description = description;
+    }
+}
+
